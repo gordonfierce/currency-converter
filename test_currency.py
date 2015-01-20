39a733d75241dbbde88882abc551bb90933f9a72
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-20 14:38:55 -0500

    Refined test for same currency conversion.

--- test_currency.py
+++ test_currency.py
@@ -5,3 +5,4 @@ rates = [("USD", "EUR", 0.86), ("BTC", "USD", 212.86)]
 
 def test_convert_to_same():
     assert cur.convert(rates, 1, "USD", "USD") == 1
+    assert cur.convert(rates, 10, "USD", "USD") == 10

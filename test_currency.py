7f9809ce862dcf81dbf6c40a7b9530d3192b7bfa
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-20 14:57:20 -0500

    Added test for basic inter-currency conversion

--- test_currency.py
+++ test_currency.py
@@ -6,3 +6,6 @@ rates = [("USD", "EUR", 0.86), ("BTC", "USD", 212.86)]
 def test_convert_to_same():
     assert cur.convert(rates, 1, "USD", "USD") == 1
     assert cur.convert(rates, 10, "USD", "USD") == 10
+
+def test_convert_from_one_to_another():
+    assert cur.convert(rates, 1, "USD", "EUR") == 0.86

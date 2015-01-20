371bda9bb0ebb9488d9f2f896ae77e13b7d55ed4
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-20 16:20:10 -0500

    Made code clunkier, whilst straining at an elegant solution.

--- currency.py
+++ currency.py
@@ -7,6 +7,22 @@ def convert(rates, value, from_string, to_string):
         return basic_convert(rates, value, from_string, to_string)
 
 
+def conversion_control_structure(rates, value, from_string, to_string):
+    if from_string == to_string:
+        return value
+    elif is_in_rates_forward(from_string, to_string):
+        return basic_convert(rates, value, from_string, to_string)
+    elif is_in_rates_backward(from_string, to_string):
+        return "Well, it ain't work"
+
+def is_in_rates_forward(from_string, to_string):
+    pass
+
+
+def is_in_rates_backward(from_string, to_string):
+    return is_in_rates_forward(to_string, from_string)
+
+
 def basic_convert(rates, value, from_string, to_string):
     rate = [rate for rate in rates if rate[0] == from_string
             and rate[1] == to_string][0]

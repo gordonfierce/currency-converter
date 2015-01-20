4d27ad73b5045b4ee8e14c73f1797311fb7c2c70
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-20 14:45:14 -0500

    Wrote a function that can convert from one currency to itself.

---
+++ currency.py
@@ -0,0 +1,7 @@
+def convert(rates, value, from_string, to_string):
+    """Given a list of conversion rates, calculate the value of one
+    currency (specified in from_string) denominated in another (to_string)"""
+    if from_string == to_string:
+        return value
+    else:
+        pass
